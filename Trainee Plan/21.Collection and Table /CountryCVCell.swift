1fc6906b0526e6e58183d61d5e83172e1afc79bc
Nebo <kindersmail08@gmail.com>
2021-10-19 15:51:34 +0300

    add model CollectionView

---
+++ Trainee Plan/21.Collection and Table /CountryCVCell.swift
@@ -0,0 +1,14 @@
+//
+//  CountryCVCell.swift
+//  Trainee Plan
+//
+//  Created by Anton Pakhomov on 19.10.2021.
+//
+
+import UIKit
+
+class CountryCVCell: UICollectionViewCell {
+    
+    let flag = UIImageView()
+    
+}
---
+++ Trainee Plan/21.Collection and Table /Data.swift
@@ -0,0 +1,15 @@
+//
+//  Data.swift
+//  Trainee Plan
+//
+//  Created by Anton Pakhomov on 19.10.2021.
+//
+
+import Foundation
+
+struct Data {
+    static let data: [(String, String, Int)] = [
+    ("Russia", "sdasds", 144000000),
+    ("Ucraine", "sdff", 30000000)]
+    
+}
