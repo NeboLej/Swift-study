f566ef80a157f86829abf2ab31c36a9a45e79a98
Nebo <kindersmail08@gmail.com>
2021-11-04 22:59:15 +0300

    finish animation

--- Trainee Plan/22.Animations/AnimationsVC.swift
+++ Trainee Plan/22.Animations/AnimationsVC.swift
@@ -62,6 +62,7 @@ class AnimationsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         //view.backgroundColor = .systemPink
         view.backgroundColor = .white
         addSubviews()
