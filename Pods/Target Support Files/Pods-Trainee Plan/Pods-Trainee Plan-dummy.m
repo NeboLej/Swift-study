33316ea661c36c74997ca03a35bf1e0b79656557
Nebo <kindersmail08@gmail.com>
2021-11-09 20:15:18 +0300

    install cocoapods and alamofire

---
+++ Pods/Target Support Files/Pods-Trainee Plan/Pods-Trainee Plan-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Trainee_Plan : NSObject
+@end
+@implementation PodsDummy_Pods_Trainee_Plan
+@end
---
+++ Trainee Plan/15.Networking/Alamofire/RestAlamofireVC.swift
@@ -0,0 +1,13 @@
+//
+//  RestAlamofireVC.swift
+//  Trainee Plan
+//
+//  Created by Anton Pakhomov on 09.11.2021.
+//
+
+import Foundation
+import Alamofire
+
+class RestAlamofireVC: UIViewController {
+    
+}
