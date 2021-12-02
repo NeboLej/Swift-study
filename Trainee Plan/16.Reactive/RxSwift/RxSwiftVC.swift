5a21575dfad04349e39329b6d4ab75335afc5093
Nebo <kindersmail08@gmail.com>
2021-12-03 02:20:38 +0300

    create instance RxSwift

--- Trainee Plan/16.Reactive/RxSwift/RxSwiftVC.swift
+++ Trainee Plan/16.Reactive/RxSwift/RxSwiftVC.swift
@@ -14,13 +14,17 @@ import RxCocoa
 
 class RxSwiftVC: UIViewController {
     
-    var isIncluded = BehaviorRelay(value: true)
+    var isIncluded = (value: true)
+    var generalText = PublishSubject<String>()
+    var time = PublishSubject<Int>()
+    
     
     private lazy var button1: UIButton = {
         var button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.backgroundColor = .systemPink
-        button.setTitle("Button", for: .normal)
+        button.setTitle("Time", for: .normal)
+        button.addTarget(self, action: #selector(stopwatch), for: .touchUpInside)
         return button
     }()
     
@@ -33,6 +37,58 @@ class RxSwiftVC: UIViewController {
         return swith
     }()
     
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = .systemBlue
+        textField.placeholder = "text"
+        textField.tintColor = .systemPink
+        textField.layer.cornerRadius = 10
+        let spacerView = UIView(frame:CGRect(x:0, y:0, width:10, height:10))
+        textField.leftViewMode = .always
+        textField.leftView = spacerView
+        return textField
+    }()
+    
+    
+    private lazy var setButton: UIButton = {
+        var button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Set", for: .normal)
+        button.layer.borderColor = .init(red: 102, green: 20, blue: 12, alpha: 1)
+        button.layer.borderWidth = 2
+        button.layer.cornerRadius = 35
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(saveUserTap), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var labelTime: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "0"
+        label.textColor = .systemPink
+        label.font = UIFont(name: "HelveticaNeue-CondensedBlack", size: 60.0)
+        
+         //instance 3
+        time.subscribe { event in
+            label.text = String(event)
+        }.disposed(by: bag)
+
+        
+        // instance 4
+//        time.filter { event in
+//            event % 2 == 0
+//        }.subscribe { event in
+//            label.text = String(event)
+//        }
+        return label
+    }()
+    
+    @objc func saveUserTap() {
+        generalText.onNext(textField.text!)
+    }
+    
     @objc func switchEvent(_ sender: UISwitch) {
         isIncluded.accept(sender.isOn)
     }
@@ -44,18 +100,49 @@ class RxSwiftVC: UIViewController {
         
         addSubviews()
         addConstrains()
-        
+        addManylabels()
         
         // instance 1
         isIncluded.asObservable().subscribe(onNext: { [weak self] value in
             self?.button1.isEnabled = value
+            self?.textField.isEnabled = value
         }).disposed(by: bag)
         
+        
+
+    }
+    
+    // instance 2
+    func addManylabels() {
+        for i in 0...10 {
+            let label = UILabel(frame: CGRect(x: 10+(i*40), y: 340, width: 100, height: 20))
+            label.textColor = .white
+            label.text = "hi"
+            generalText.subscribe { string in
+                label.text = string
+            } onError: { error in
+                label.text = "Error"
+            }.disposed(by: bag)
+            view.addSubview(label)
+
+        }
+    }
+    
+    @objc func stopwatch() {
+        for i in 1...60 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.time.onNext(i)
+                print(i)
+            }
+        }
     }
     
     private func addSubviews() {
         view.addSubview(button1)
         view.addSubview(swith)
+        view.addSubview(textField)
+        view.addSubview(setButton)
+        view.addSubview(labelTime)
     }
     
     private func addConstrains() {
@@ -68,7 +155,20 @@ class RxSwiftVC: UIViewController {
             swith.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50.0),
             swith.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20.0),
             swith.heightAnchor.constraint(equalToConstant: 30.0),
-            swith.widthAnchor.constraint(equalToConstant: 60.0)
+            swith.widthAnchor.constraint(equalToConstant: 60.0),
+            
+            textField.topAnchor.constraint(equalTo: swith.bottomAnchor, constant: 30.0),
+            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10.0),
+            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10.0),
+            textField.heightAnchor.constraint(equalToConstant: 30.0),
+            
+            setButton.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 30.0),
+            setButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            setButton.heightAnchor.constraint(equalToConstant: 70.0),
+            setButton.widthAnchor.constraint(equalToConstant: 70.0),
+            
+            labelTime.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            labelTime.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 100)
             
         ])
     }
