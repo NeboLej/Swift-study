46cd02d239ed7f2bc394af881209ba7c91ad422a
Nebo <kindersmail08@gmail.com>
2021-12-07 01:38:45 +0300

    change sample SOLID

--- Trainee Plan/42.SOLID/SOLID.swift
+++ Trainee Plan/42.SOLID/SOLID.swift
@@ -9,31 +9,205 @@ import Foundation
 import UIKit
 
 
-//MARK - S
-class S {
-    var view = UIView()
+//MARK:  - S
+// not ok
+class Car {
+    let motor = Motor()
+    let radio = Radio()
     
-    //not good
-    private func createLabel() {
-        let label = UILabel()
-        label.text = "wtf"
-        view.addSubview(label)
+    func spinTheWheels() {
+        //крутим колеса
     }
     
-    //good
-    private func createLabel2()->UILabel {
-        let label = UILabel()
-        label.text = "wtf"
-        return label
+    func playMusic() {
+        //включаем музыку
+    }
+}
+class Motor {
+    
+}
+
+class Radio {
+    
+}
+
+//ok
+class Car1 {
+    let motor1 = Motor1()
+    let radio1 = Radio1()
+    
+    func drive() {
+        motor1.spinTheWheels()
     }
     
-    private func addLabel() {
-        let label = createLabel2()
-        view.addSubview(label)
+    func playMusic() {
+        radio1.sing()
+    }
+}
+
+class Motor1 {
+    func spinTheWheels() {
+        //крутим колеса
+    }
+}
+class Radio1 {
+    func sing() {
+        //включаем музыку
+    }
+}
+
+
+//MARK: - O
+
+protocol MusicalInstruments {
+    func play()
+}
+class Orchestra {
+    let composition: [MusicalInstruments] = [Flute(), Drum(), Violin(), Violin(), Violin() ]
+    
+    func playMelody(){
+        composition.forEach { instruments in
+            instruments.play()
+        }
     }
     
 }
-//MARK - O
-class O {
-    
+
+class Flute: MusicalInstruments {
+    func play() {
+        print("tyyyyyy tyyyy")
+    }
 }
+
+class Drum: MusicalInstruments {
+    func play() {
+        print("bom bom tsss")
+    }
+}
+
+class Violin: MusicalInstruments {
+    func play() {
+        print("iiiiiii")
+    }
+}
+
+
+//MARK: - L
+
+protocol Programmer {
+    func writeCode() -> String
+}
+
+class Junior: Programmer {
+    func writeCode() -> String {
+        return "я это сделаю!"
+    }
+}
+
+class Senior: Programmer {
+    func writeCode() -> String {
+        return "это не входит в текущую задачу"
+    }
+}
+
+class TeamLead {
+    func issueTask(worker: Programmer) {
+        print(worker.writeCode())
+    }
+}
+
+class Boss: TeamLead {
+    func dismissWorker(worker: Programmer) {
+        print("you dismiss!")
+    }
+}
+
+class Company {
+    let oleg = Junior()
+    let petr = Senior()
+    let dima = TeamLead()
+    let hugo = Boss()
+    
+    func workProcess() {
+        hugo.issueTask(worker: oleg)
+        dima.issueTask(worker: oleg)
+        hugo.issueTask(worker: petr)
+        dima.issueTask(worker: petr)
+        
+        hugo.dismissWorker(worker: petr)
+    }
+
+}
+
+//MARK: I
+// NOT OK
+protocol Musician {
+    func sing()
+    func playAnInstrument()
+    func makeBeat()
+}
+
+class Oxxxy: Musician {
+    func sing() {
+        print("МОХ!")
+    }
+    
+    func playAnInstrument() {
+        print("Я не умею!")
+    }
+    
+    func makeBeat() {
+        print("куплю бит, от 10 минут!")
+    }
+}
+
+//OK
+protocol Signer {
+    func sign()
+}
+protocol Instrumental {
+    func playAnInstrument()
+}
+protocol Beatmaker {
+    func makeBeat()
+}
+
+class LedyGaga: Signer, Instrumental {
+    func sign() {
+        print("ga ga ylalay")
+    }
+    func playAnInstrument() {
+        print("А ЛЕГКО!")
+    }
+}
+class Petya12YearsOld: Beatmaker {
+    func makeBeat() {
+        print("туц туц")
+    }
+}
+
+//MARK: D
+
+protocol Artist { }
+extension LedyGaga: Artist { }
+extension Petya12YearsOld: Artist { }
+
+protocol Event {
+  func perform(object: Artist)
+}
+
+class CityDay: Event {
+    func perform(object: Artist) {
+
+    }
+}
+
+class User {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
+
+
