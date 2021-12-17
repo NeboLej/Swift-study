e666ce24adfd8643b0a348e230d082fad0281a6b
Nebo <kindersmail08@gmail.com>
2021-12-17 04:21:11 +0300

    finish GCD and operation

--- Trainee Plan/Multithreading/GCDVC.swift
+++ Trainee Plan/Multithreading/GCDVC.swift
@@ -6,7 +6,7 @@ class GCDVC: UIViewController {
     lazy var button1: UIButton = {
         var button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = UIColor(displayP3Red: CGFloat.random(in: 0...1 ), green: CGFloat.random(in: 0...1 ), blue: CGFloat.random(in: 0...1 ), alpha: 1)
+        button.backgroundColor = .systemPurple
         button.addTarget(self, action: #selector(simpleMethod), for: .touchUpInside)
         button.setTitle("Simple Method", for: .normal)
         return button
@@ -14,7 +14,7 @@ class GCDVC: UIViewController {
     lazy var button2: UIButton = {
         var button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = UIColor(displayP3Red: CGFloat.random(in: 0...1 ), green: CGFloat.random(in: 0...1 ), blue: CGFloat.random(in: 0...1 ), alpha: 1)
+        button.backgroundColor = .systemPurple
         button.addTarget(self, action: #selector(simpleMethodGlobalQueue), for: .touchUpInside)
         button.setTitle("Simple Method GlobalQueue", for: .normal)
         return button
@@ -22,7 +22,7 @@ class GCDVC: UIViewController {
     lazy var button3: UIButton = {
         var button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = UIColor(displayP3Red: CGFloat.random(in: 0...1 ), green: CGFloat.random(in: 0...1 ), blue: CGFloat.random(in: 0...1 ), alpha: 1)
+        button.backgroundColor = .systemPurple
         button.addTarget(self, action: #selector(cycle), for: .touchUpInside)
         button.setTitle("Simple Cycle", for: .normal)
         return button
@@ -30,7 +30,7 @@ class GCDVC: UIViewController {
     lazy var button4: UIButton = {
         var button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = UIColor(displayP3Red: CGFloat.random(in: 0...1 ), green: CGFloat.random(in: 0...1 ), blue: CGFloat.random(in: 0...1 ), alpha: 1)
+        button.backgroundColor = .systemPurple
         button.addTarget(self, action: #selector(multiCycle), for: .touchUpInside)
         button.setTitle("Concurrent Cycle", for: .normal)
         return button
@@ -38,7 +38,72 @@ class GCDVC: UIViewController {
     lazy var button5: UIButton = {
         var button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.backgroundColor = UIColor(displayP3Red: CGFloat.random(in: 0...1 ), green: CGFloat.random(in: 0...1 ), blue: CGFloat.random(in: 0...1 ), alpha: 1)
+        button.backgroundColor = .systemPurple
+        button.addTarget(self, action: #selector(delayedLaunch), for: .touchUpInside)
+        button.setTitle("delayed Launch", for: .normal)
+        return button
+    }()
+    
+    lazy var button6: UIButton = {
+        var button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemPurple
+        button.addTarget(self, action: #selector(inactiveQueue), for: .touchUpInside)
+        button.setTitle("inactiveQueue", for: .normal)
+        return button
+    }()
+    
+    lazy var button7: UIButton = {
+        var button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemPurple
+        button.addTarget(self, action: #selector(semaphore1), for: .touchUpInside)
+        button.setTitle("semaphore1", for: .normal)
+        return button
+    }()
+    
+    lazy var button8: UIButton = {
+        var button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemPurple
+        button.addTarget(self, action: #selector(semaphore3), for: .touchUpInside)
+        button.setTitle("semaphore3", for: .normal)
+        return button
+    }()
+    
+    lazy var button9: UIButton = {
+        var button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemPurple
+        button.addTarget(self, action: #selector(semaphore10), for: .touchUpInside)
+        button.setTitle("semaphore10", for: .normal)
+        return button
+    }()
+    
+    lazy var button10: UIButton = {
+        var button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemPurple
+        button.addTarget(self, action: #selector(dispatchGrop), for: .touchUpInside)
+        button.setTitle("Group", for: .normal)
+        return button
+    }()
+    
+    lazy var button11: UIButton = {
+        var button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemPurple
+        button.addTarget(self, action: #selector(operationOne), for: .touchUpInside)
+        button.setTitle("Operation 1", for: .normal)
+        return button
+    }()
+    
+    lazy var button12: UIButton = {
+        var button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemPurple
+        button.addTarget(self, action: #selector(operationTwo), for: .touchUpInside)
+        button.setTitle("Operation 2", for: .normal)
         return button
     }()
     
@@ -68,6 +133,13 @@ class GCDVC: UIViewController {
         view.addSubview(button3)
         view.addSubview(button4)
         view.addSubview(button5)
+        view.addSubview(button6)
+        view.addSubview(button7)
+        view.addSubview(button8)
+        view.addSubview(button9)
+        view.addSubview(button10)
+        view.addSubview(button11)
+        view.addSubview(button12)
         view.addSubview(langSigmentsControl)
     }
     
@@ -97,6 +169,41 @@ class GCDVC: UIViewController {
             button5.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             button5.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             button5.heightAnchor.constraint(equalToConstant: 30),
+            
+            button6.topAnchor.constraint(equalTo: button5.bottomAnchor, constant: 10.0),
+            button6.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            button6.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            button6.heightAnchor.constraint(equalToConstant: 30),
+            
+            button7.topAnchor.constraint(equalTo: button6.bottomAnchor, constant: 10.0),
+            button7.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            button7.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            button7.heightAnchor.constraint(equalToConstant: 30),
+            
+            button8.topAnchor.constraint(equalTo: button7.bottomAnchor, constant: 10.0),
+            button8.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            button8.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            button8.heightAnchor.constraint(equalToConstant: 30),
+            
+            button9.topAnchor.constraint(equalTo: button8.bottomAnchor, constant: 10.0),
+            button9.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            button9.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            button9.heightAnchor.constraint(equalToConstant: 30),
+            
+            button10.topAnchor.constraint(equalTo: button9.bottomAnchor, constant: 10.0),
+            button10.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            button10.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            button10.heightAnchor.constraint(equalToConstant: 30),
+            
+            button11.topAnchor.constraint(equalTo: button10.bottomAnchor, constant: 10.0),
+            button11.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            button11.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            button11.heightAnchor.constraint(equalToConstant: 30),
+            
+            button12.topAnchor.constraint(equalTo: button11.bottomAnchor, constant: 10.0),
+            button12.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            button12.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            button12.heightAnchor.constraint(equalToConstant: 30),
             
             langSigmentsControl.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40.0),
             langSigmentsControl.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -139,4 +246,156 @@ class GCDVC: UIViewController {
         }
     }
     
+    @objc func delayedLaunch() { //отложенный запуск
+        let second = 5
+        afterBlock(second: second) {
+            print("прошло \(second) секунд")
+        }
+    }
+    
+    func afterBlock(second: Int, queue: DispatchQueue =
+                    DispatchQueue.global(), complition: @escaping ()->()) {
+        queue.asyncAfter(deadline: .now() + .seconds(second)) {
+            complition()
+        }
+    }
+    
+    @objc func inactiveQueue() {
+        let inactiveQueue = DispatchQueue(label: "Inactive", attributes: [.concurrent, .initiallyInactive])
+        
+        inactiveQueue.async {
+            print("Done")
+        }
+        print("Not yet startet...")
+        inactiveQueue.activate()
+    }
+    
+    @objc func semaphore1() {
+        print("_______________________")
+        semaphoreExample(threads: 1)
+    }
+    @objc func semaphore3() {
+        print("_______________________")
+        semaphoreExample(threads: 3)
+    }
+    @objc func semaphore10() {
+        print("_______________________")
+        semaphoreExample(threads: 10)
+    }
+    
+    func semaphoreExample(threads: Int) {
+        let queue = DispatchQueue(label: "queue", attributes: .concurrent)
+        let sepahore = DispatchSemaphore(value: threads)
+        
+        queue.async {
+            sepahore.wait() // -1
+            sleep(2)
+            print("method 1")
+            sepahore.signal() // +1
+        }
+        queue.async {
+            sepahore.wait() // -1
+            sleep(2)
+            print("method 2")
+            sepahore.signal() // +1
+        }
+        queue.async {
+            sepahore.wait() // -1
+            sleep(2)
+            print("method 3")
+            sepahore.signal() // +1
+        }
+        queue.async {
+            sepahore.wait() // -1
+            sleep(2)
+            print("method 4")
+            sepahore.signal() // +1
+        }
+    }
+    
+    
+    @objc func dispatchGrop() {
+        download()
+    }
+    
+    func download() {
+        let queue = DispatchQueue(label: "q", attributes: .concurrent)
+        let groupOne = DispatchGroup()
+        
+        groupOne.enter()
+        queue.async {
+            sleep(2)
+            print("one")
+            groupOne.leave()
+        }
+        groupOne.enter()
+        queue.async {
+            sleep(5)
+            print("two")
+            groupOne.leave()
+        }
+        groupOne.enter()
+        queue.async {
+            sleep(3)
+            print("three")
+            groupOne.leave()
+        }
+        
+        groupOne.wait()
+        print("Finish all")
+        
+        
+        groupOne.notify(queue: .main) {
+            print("все задачи выполнены а эта выполняется в \(Thread.current)")
+        }
+        
+    }
+    
+    
+    
+    @objc func operationOne() {
+
+        print(Thread.current)
+        var string = "__"
+        let operation = BlockOperation {
+            string = "he he he"
+            print(Thread.current)
+        }
+        
+        let queue = OperationQueue() //автоматически создает канкарент очередь
+        queue.addOperation(operation)
+        sleep(1)
+        print(string)
+    }
+    
+    @objc func operationTwo() {
+
+        print(Thread.current)
+ 
+        let operation1 = BlockOperation {
+            sleep(1)
+            print("operation1")
+            print(Thread.current)
+        }
+        
+        let operation2 = BlockOperation {
+            sleep(6)
+            print("operation2")
+            print(Thread.current)
+        }
+        
+        let operation3 = BlockOperation {
+            sleep(3)
+            print("operation3")
+            operation2.cancel()
+            print(operation2.isCancelled)
+            print(Thread.current)
+        }
+        
+        let queue = OperationQueue()
+        queue.addOperation(operation1)
+        queue.addOperation(operation2)
+        queue.addOperation(operation3)
+        //operation2.cancel()
+    }
 }
